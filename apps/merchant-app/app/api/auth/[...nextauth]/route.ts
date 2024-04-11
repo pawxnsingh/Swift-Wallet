4f8842d0adfe3d5b8a81aeb40ef05c16e37958ac
Pawan Singh Dogra <psdograofficial@gmail.com>
2024-04-11 07:47:57 +0530

    dasasdasd

--- apps/merchant-app/app/api/auth/[...nextauth]/route.ts
+++ apps/merchant-app/app/api/auth/[...nextauth]/route.ts
@@ -3,5 +3,6 @@ import { authOptions } from "../../../../lib/auth";
 
 // @ts-ignore
 const handler = NextAuth(authOptions);
+// this is the change i have made
 
 export { handler as GET, handler as POST };
